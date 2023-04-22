f72a399f6452bbc9107e173ddc6f90fe3719f5af
liupengyuan <lpy962464@163.com>
2023-04-22 15:31:17 +0800

    app init

---
+++ src/test/java/com/roker/oss/OssSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roker.oss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
